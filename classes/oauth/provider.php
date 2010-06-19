9b7da72549ec9f4d6b709ac1e66514c5a0b256de
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-19 22:39:23 +0800

    Added OAuth_Provider

---
+++ classes/oauth/provider.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * OAuth Provider
+ *
+ * @package    Kohana/OAuth
+ * @package    Base
+ * @author     Kohana Team
+ * @copyright  (c) 2010 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+abstract class OAuth_Provider {
+
+} // End OAuth_Signature
