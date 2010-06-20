d7aa06afc6821b201ea9b83564c8b5b4863ad47a
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-20 09:44:33 +0800

    Do not include the Kohana consumer details

--- config/oauth.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-	'twitter' => array(
-		'key'    => 'fRKnjCvs3aR9YhTFuZds1A',
-		'secret' => 'dAbe6LkTL7S2XKpbzVtrnyiHvOKViyJVGyTfFbj8lSw',
-	),
-);
