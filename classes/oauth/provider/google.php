28d6cbc722f59b8e00b4309ecf2c7c375fe6b614
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-24 09:44:49 +0800

    Added user_profile API call, gets the Google Profile for the authed user

--- classes/oauth/provider/google.php
+++ classes/oauth/provider/google.php
@@ -24,4 +24,18 @@ class OAuth_Provider_Google extends OAuth_Provider {
 		),
 	);
 
+	public function user_profile(OAuth_Consumer $consumer, OAuth_Token_Access $token, $format = 'json')
+	{
+		$request = OAuth_Request::factory('resource', 'GET', "http://www-opensocial.googleusercontent.com/api/people/@me/@self", array(
+			'oauth_consumer_key' => $consumer->key,
+			'oauth_token'        => $token->token,
+		));
+
+		// Sign the request using only the consumer, no token is available yet
+		$request->sign($this->signature, $consumer, $token);
+
+		// Return the response
+		return $request->execute();
+	}
+
 } // End OAuth_Provider_Google
