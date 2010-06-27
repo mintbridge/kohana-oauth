3634ee75bc206452e378259b473a6c3cf1f105cd
Woody Gilk <woody.gilk@kohanaphp.com>
2010-06-27 11:39:32 +0800

    Completed comments for OAuth_Consumer

--- classes/oauth/consumer.php
+++ classes/oauth/consumer.php
@@ -10,17 +10,40 @@
  */
 class OAuth_Consumer {
 
+	/**
+	 * Create a new consumer object.
+	 *
+	 *     $consumer = OAuth_Consumer::factory($options);
+	 *
+	 * @param   array  consumer options, key and secret are required
+	 * @return  OAuth_Consumer
+	 */
 	public static function factory(array $options = NULL)
 	{
 		return new OAuth_Consumer($options);
 	}
 
+	/**
+	 * @var  string  consumer key
+	 */
 	protected $key;
 
+	/**
+	 * @var  string  consumer secret
+	 */
 	protected $secret;
 
+	/**
+	 * @var  string  callback URL for OAuth authorization completion
+	 */
 	protected $callback;
 
+	/**
+	 * Sets the consumer key and secret.
+	 *
+	 * @param   array  consumer options, key and secret are required
+	 * @return  void
+	 */
 	public function __construct(array $options = NULL)
 	{
 		if ( ! isset($options['key']))
@@ -45,14 +68,31 @@ class OAuth_Consumer {
 		}
 	}
 
+	/**
+	 * Return the value of any protected class variable.
+	 *
+	 *     // Get the consumer key
+	 *     $key = $consumer->key;
+	 *
+	 * @param   string  variable name
+	 * @return  mixed
+	 */
 	public function __get($key)
 	{
 		return $this->$key;
 	}
 
+	/**
+	 * Change the consumer callback.
+	 *
+	 * @param   string  new consumer callback
+	 * @return  $this
+	 */
 	public function callback($callback)
 	{
 		$this->callback = $callback;
+
+		return $this;
 	}
 
 } // End OAuth_Consumer
