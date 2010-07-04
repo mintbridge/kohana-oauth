88537cf14859e26200f3f6f65027cbed3d2c28ce
Woody Gilk <woody.gilk@kohanaphp.com>
2010-07-04 21:38:06 +0800

    We no longer need init.php, the demos have been removed

--- init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-Route::set('oauth', 'oauth(/<controller>(/<action>))')
-	->defaults(array(
-		'directory'  => 'oauth',
-		'controller' => 'demo',
-	));
